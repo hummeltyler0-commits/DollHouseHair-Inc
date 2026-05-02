7ed75ef750e20e28bcdc23a309643b62009d068a
hummeltyler0-commits <hummel.tyler0@gmail.com>
2026-05-02 00:46:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -131,7 +131,6 @@ app.get("/api/availability", (req, res) => {
 
   res.json({ slots });
 });
-, (req, res) => {
   const date = req.query.date;
   if (!date) return res.status(400).json({ error: "Date required." });
   res.json({ date, slots: slotsForDate(date) });
